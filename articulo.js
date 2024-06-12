ae2a9a4ddac5bf5e84c23824e72afb54791e529a
PresaMaite <m.presa@hotmail.com>
2024-06-12 17:55:29 +0200

    feat(card): card skeleton

--- articulo.js
+++ articulo.js
@@ -6,8 +6,33 @@ fetch(URL)
         console.log(json)
 
         json.map((cloth) => {
+            const card = document.createElement("li");
+            
+            const cardImage = card.appendChild(document.createElement("img"));
+            cardImage.src = cloth.image;
+            cardImage.height = "100";
+            cardImage.width = "100";
+
+            const cardContent = card.appendChild(document.createElement("div"));
+
+            let contentTitle = cardContent.appendChild(document.createElement("h3"));
+            contentTitle.innerText = cloth.title;
+
+            let contentDescription = cardContent.appendChild(document.createElement("p"));
+            contentDescription.innerText = cloth.description;
+
+            const contentButtons = document.createElement("div");
+            let contentPrice = contentButtons.appendChild(document.createElement("p"));
+            contentPrice.innerText = `${cloth.price}€`;
+
+            const contentBtn = contentButtons.appendChild(document.createElement("button"));
+            contentBtn.innerText = "Añadir";
+
+            cardContent.appendChild(contentButtons);
 
 
+
+            document.getElementById("cardsContainer").appendChild(card);
         })
 
     });
