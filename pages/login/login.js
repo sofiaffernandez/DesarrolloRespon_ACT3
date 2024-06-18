11921e81dd6a69c1363604783c60d0b97972b99a
unknown <sofiaffeijoo98@gmail.com>
2024-06-18 21:08:18 +0200

    se añade formulario datos, css y se arregllan algunas cosas de funcionamiento

--- pages/login/login.js
+++ pages/login/login.js
@@ -3,22 +3,27 @@ document.addEventListener("DOMContentLoaded", function() {
     const registerForm = document.querySelector("#registerForm");
 
     if (loginForm) {
-        handleForm(loginForm, "../home/home.html");
+        handleForm(loginForm);
     }
 
     if (registerForm) {
-        handleForm(registerForm, ".../home/home.html");
+        handleForm(registerForm);
     }
 
-    function handleForm(form, redirectUrl) {
+    function handleForm(form) {
         form.addEventListener("submit", function(event) {
             event.preventDefault();
 
             const email = form.querySelector("[name='email']").value;
-            const password = form.querySelector("[name='password']").value;
+            const contraseña = form.querySelector("[name='contraseña']").value;
             const rememberMe = form.querySelector("[name='rememberMe']") ? form.querySelector("[name='rememberMe']").checked : false;
-            const name = form.querySelector("[name='name']") ? form.querySelector("[name='name']").value : null;
-            const lastName = form.querySelector("[name='lastName']") ? form.querySelector("[name='lastName']").value : null;
+            const nombre = form.querySelector("[name='nombre']") ? form.querySelector("[name='nombre']").value : null;
+            const apellidos = form.querySelector("[name='apellidos']") ? form.querySelector("[name='apellidos']").value : null;
+            console.log("Email:", email);
+            console.log("Contraseña:", contraseña);
+            console.log("Recordarme:", rememberMe);
+            console.log("Nombre:", nombre);
+            console.log("Apellidos:", apellidos);
 
             // Expresión regular para validar el formato del email
             const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
@@ -30,29 +35,29 @@ document.addEventListener("DOMContentLoaded", function() {
                 return;
             }
 
-            if (!passwordPattern.test(password)) {
+            if (!passwordPattern.test(contraseña)) {
                 showToast("errorToast", "La contraseña debe tener al menos una letra, un número y al menos 6 caracteres.");
                 return;
             }
 
-            if (form.id === 'registerForm' && (!name || !lastName)) {
+            if (form.id === 'registerForm' && (!nombre || !apellidos)) {
                 showToast("errorToast", "Por favor, rellena todos los campos.");
                 return;
             }
 
-            if (email && password && (form.id !== 'registerForm' || (name && lastName))) {
-                sessionStorage.setItem("userEmail", email);
+            if (email && contraseña && (form.id !== 'registerForm' || (nombre && apellidos))) {
+                sessionStorage.setItem("email", email);
                 sessionStorage.setItem("rememberMe", rememberMe);
                 
                 if (form.id === 'registerForm') {
-                    sessionStorage.setItem("userName", name);
-                    sessionStorage.setItem("userLastName", lastName);
+                    sessionStorage.setItem("nombre", nombre);
+                    sessionStorage.setItem("apellidos", apellidos);
                 }
 
-                showToast("successToast", "Operación exitosa.");
+                showToast("successToast", "Registro correcto.");
                 setTimeout(() => {
-                    window.location.href = redirectUrl; // Redirecciona a la página correspondiente
-                }, 1500); // Espera 1.5 segundos antes de redirigir
+                    window.location.href = "../home/home.html" ; 
+                }, 1000); 
             } else {
                 showToast("errorToast", "Por favor, rellena todos los campos.");
             }
